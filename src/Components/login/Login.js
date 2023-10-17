35714d1b483479f3eb227744279769428633bf30
Denys Malieiev <denysmalieiev@gmail.com>
2023-10-17 20:32:05 +0530

    updating...

--- src/Components/login/Login.js
+++ src/Components/login/Login.js
@@ -11,7 +11,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('/api/sin', {
+      const response = await fetch('/api/sin', { //fetch from backend
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
